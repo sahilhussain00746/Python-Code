1e18646b1e463b4c679d96dc22e757123d0bb1fa
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-03 09:54:47 +0530

    first commit

---
+++ Day 2/task7_mathematical.py
@@ -0,0 +1,15 @@
+print(123+456)
+print(2-1)
+print(4*2)
+print(6/3) #this will return float type
+print(6//3) #now this will return int type
+print(2**2) #This works like a power
+
+
+#Question file BMI (Body mass index)
+height = 1.65
+weight = 84
+
+height = height ** 2 #this is doing power 2
+bmi = weight/height
+print(bmi)
