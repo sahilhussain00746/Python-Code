45855736bb2eac0373dec065cb7774a11a8a1d53
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-03 18:40:13 +0530

    commit

--- Day 2/task7_mathematical.py
+++ Day 2/task7_mathematical.py
@@ -6,10 +6,3 @@ print(6//3) #now this will return int type
 print(2**2) #This works like a power
 
 
-#Question file BMI (Body mass index)
-height = 1.65
-weight = 84
-
-height = height ** 2 #this is doing power 2
-bmi = weight/height
-print("Hence the bmi is " + str(bmi))
---
+++ Day 2/task8_question.py
@@ -0,0 +1,8 @@
+#Question file BMI (Body mass index)
+height = 1.65
+weight = 84
+
+height = height ** 2 #this is doing power 2
+bmi = weight/height
+print("Hence the bmi is " + str(bmi)) #It is print the flaot which very large number after decimal point
+print(round(bmi)) #now it will print the round up value
