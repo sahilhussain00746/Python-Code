31e200b2aa2fd02ff64ceed08dcd0f0c545aa437
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-03 19:35:12 +0530

    commit

--- Day 3/task11_conditional_operator.py
+++ Day 3/task11_conditional_operator.py
@@ -1,1 +1,7 @@
 #This is about conditional statement
+print("Welcome to the rollar  coaster")
+age = int(input("Enter your age: "))
+if age>=18:
+    print("You can ride on the rollar coaster")
+else:
+    print("No, You are below 18")
