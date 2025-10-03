635bfde4da45cd625f4d0fe3b935c9f458c15c62
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-03 19:22:23 +0530

    commit

--- Day 2/task9_f_string.py
+++ Day 2/task9_f_string.py
@@ -1,7 +1,7 @@
 score = 0
 time = 10.5
 
-print("Yor score is " + str(score) + "Your height is " + str(height)) 
+print("Yor score is " + str(score) + "Your height is " + str(time)) 
 
 #instead of doing this you can do
 #this is called f_string
