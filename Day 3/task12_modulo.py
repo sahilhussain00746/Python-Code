671a02664e88edc73dcaa84d220f6f71d602f87b
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-05 17:00:20 +0530

    commit

---
+++ Day 3/task12_modulo.py
@@ -0,0 +1,6 @@
+#We use modulo to find the remainder
+num = int(input("Enter a number to check it is even or odd: "))
+if(num % 2==0):
+    print("The given number is even.")
+else:
+    print("The given number is odd.")
---
+++ Day 3/task13_nested_if.py
@@ -0,0 +1,9 @@
+print("Welcome to the rollar coaster")
+age = int(input("Enter Your age: "))
+if age>=18:
+    if age<=24:
+        print("Ticket price for you is 12$")
+    else:
+        print("Ticket price for you is 15$ ")
+else:
+    print("You have no permission to ride on the rollar coaster")
