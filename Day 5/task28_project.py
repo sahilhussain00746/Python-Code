34c380381634008d62bfef1e0e9a2794acd6faec
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-15 17:58:53 +0530

    commit

--- Day 5/task28_project.py
+++ Day 5/task28_project.py
@@ -1,3 +1,5 @@
+import random
+
 print("Welcome to the Password Generator")
 
 alphabet = [
@@ -5,7 +7,7 @@ alphabet = [
 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 symbols = [
 "`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "|", "\\", ":", ";", "\"", "'", "<", ">", ",", ".", "?", "/"
@@ -15,7 +17,36 @@ length_no = int(input("Entr the length of your password: "))
 symbol_no = int(input("How many symbols should there: "))
 numbers_no = int(input("How many numbers should there: "))
 
-pass_list = [alphabet, number, symbols]
+# password = ""
 
-for doing in range(1, length_no):
+# for doing in range(0, length_no):
+#     password += random.choice(alphabet)
+
+# for doing in range(0, symbol_no):
+#     password += random.choice(symbols)
+
+# for doing in range(0, numbers_no):
+#     password += random.choice(str(number))
     
+# print(password) #This will print the simple password in order
+
+password_list = []
+
+for doing in range(0, length_no):
+    password_list.append(random.choice(alphabet))
+
+for doing in range(0, symbol_no):
+    password_list.append(random.choice(symbols))
+
+for doing in range(0, numbers_no):
+    password_list.append(random.choice(number))
+    
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+ 
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
