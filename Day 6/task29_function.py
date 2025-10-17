048ef7eda74613ead38d2f4b61d77d1bc849f792
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-10-17 19:50:10 +0530

    commit

---
+++ Day 6/task29_function.py
@@ -0,0 +1,11 @@
+# funtion
+# len()
+# round() These are some inbuilt function
+# print()
+
+#user defined function
+def my_function():
+    print("Hello Sahil")
+    
+my_function() #here i am calling the function
+#without calling the function it doesn't return anything
---
+++ Day 6/task30_while_loop.py
@@ -0,0 +1,10 @@
+#while loop
+number = 3
+while number != 0:
+    print("Hello World")
+    number -= 1
+  
+unnumber = 2  
+while not unnumber == 0:
+    print("Thisis ended")
+    unnumber -= 1
