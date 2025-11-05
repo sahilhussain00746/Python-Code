2f6250e9c77a5d364be5e0261f3aa38a0e8197de
Sahil Hussain <sahilhussain00746@users.noreply.github.com>
2025-11-05 18:23:45 +0530

    commit

--- Day 7/task32_random_word.py
+++ Day 7/task32_random_word.py
@@ -1,17 +1,95 @@
+hangman_pic= [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''',
+    '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''',
+    '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+========='''
+]
+
+
+
+
 import random
 word = ["lion", "tiger", "camel"]
 
-random_word = random.choice(word)
+random_word = random.choice(word) #creating random word 
 print(random_word)
 
-guess = input("Enter a letter: ").lower()
+placeholder = "" #creating empty space
+for position in range(len(random_word)): 
+    placeholder += "_"
+    
+print(placeholder)
 
-guess_no = 0
-for letter in random_word:
-    if guess == letter:
-        guess_no = 1
+live = 6
+game_over = False 
+correct_letter = [] # creating a new list to store letter
 
-if(guess_no == 1):
-    print("Correct letter")
-else: 
-    print("Wrong letter")
+while not game_over:  #running the loop while it is false
+    guess = input("Enter a letter: ").lower() #taking input from user
+
+    display = ""
+    for letter in random_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(letter) #putting the letter to remember and checking for another time when user gives the input
+        elif letter in correct_letter: #checking the letter in available in the correct_list
+            display += letter
+        else:
+            display += "_"
+        
+    if guess not in correct_letter:
+        live -= 1
+        if live == 0:
+            game_over = True
+            print("You lose")
+        
+    if "_" not in display:
+        game_over = True
+        print("You win")      
+        
+        
+    print(hangman_pic[live])
